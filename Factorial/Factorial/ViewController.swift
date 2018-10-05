6f6fdc1dbe0e64a2f88cbca8e715169645a1822b
Mnats <mnats@buymie.eu>
2018-10-05 15:10:55 +0400

    Initioan commit

---
+++ Factorial/Factorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Factorial
+//
+//  Created by Mnats on 10/5/18.
+//  Copyright © 2018 Mnats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
